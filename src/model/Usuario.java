7e8e95532c22eb6d8e833bfe0ac8fd28fc994534
Santiago <santiago.fierro4@outlook.cl>
2019-05-17 09:08:37 -0400

    Definida la estructura del usuario (Temporal).

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -6,9 +6,76 @@
 package model;
 
 /**
- *
+ * Clase que define nuestros usuarios.
  * @author Santiago
  */
 public class Usuario {
+    
+    // Atributos
+    private String nombre;
+    private String apellido;
+    private String username;
+    private String password;
+    private String genero;
+    private int edad;
+
+    // Constructor
+    public Usuario(String nombre, String apellido, String username, String password, String genero, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.username = username;
+        this.password = password;
+        this.genero = genero;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
     
 }
